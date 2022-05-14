d69cff2e83e0901d21cc5e5cd4c5f211b8d031af
ksyrba <ksyrbaba0812@gmail.com>
2022-05-14 05:27:28 +0000

    [add]authenticate

--- app/controllers/public/customer_communities_controller.rb
+++ app/controllers/public/customer_communities_controller.rb
@@ -1,4 +1,6 @@
 class Public::CustomerCommunitiesController < ApplicationController
+  
+  
   def create
     @customer_community = CustomerCommunity.new(customer_id: current_customer.id, community_id: params[:community_id])
     if @customer_community.save
--- app/controllers/public/registration_communities_controller.rb
+++ app/controllers/public/registration_communities_controller.rb
@@ -1,6 +1,7 @@
 class Public::RegistrationCommunitiesController < ApplicationController
+  before_action :authenticate_customer!
+  
   def index
-    @communities = Community.page(params[:page]).where()
     @communities = current_customer.communities
   end
 end
