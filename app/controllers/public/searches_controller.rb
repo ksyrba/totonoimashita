e4bbc51beba3b96663140daf5af86d2a9088c828
ksyrba <ksyrbaba0812@gmail.com>
2022-05-21 12:34:45 +0000

    [fix]search layout

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -1,14 +1,12 @@
 class Public::SearchesController < ApplicationController
 
   def search_community
-    # binding.pry
+    @word = params[:word]
     @search_result = Community.search(params[:word])
     @communities = Community.page(params[:page]).per(20).order(created_at: :desc)
   end
 
   def search_area
-    # binding.pry
-    # @community = Community.find_by(area_id: params[:area_id])
     @communities = Community.where(area_id: params[:area_id]).page(params[:page]).per(20).order(created_at: :desc)
   end
 end
