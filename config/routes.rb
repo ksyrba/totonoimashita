c97b2f85e1eb137a12041548c353c03b6e8e01d1
ksyrba <ksyrbaba0812@gmail.com>
2022-05-29 07:46:18 +0000

    [modified]action name

--- config/routes.rb
+++ config/routes.rb
@@ -28,8 +28,7 @@ Rails.application.routes.draw do
   end
 
 # 管理者
-  devise_for :admin, skip: [:passwords] ,controllers: {
-    registrations: "admin/registrations",
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
   namespace :admin do
